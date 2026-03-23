e65bc4362706ce56a0534b27c2171ceac1d5ba79
DonAdam2 <adam.morsi@yahoo.com>
2026-03-23 16:47:43 +0200

    remove IE support

--- src/index.jsx
+++ src/index.jsx
@@ -3,8 +3,6 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 //import meta image
 import '@/public/assets/images/metaImage.jpg';
-// required for babel polyfills
-import 'regenerator-runtime/runtime';
 //root component
 import App from './App';
 //styles
